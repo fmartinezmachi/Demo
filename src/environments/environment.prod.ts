cf11cb48178730f0bc58647ea6016718de80b856
Martinez Machi <fdmz@gft.com>
2019-12-02 12:24:12 +0100

    fixed build errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const baseApi = 'globilehub-vm.westeurope.cloudapp.azure.com:8085/globileHub/';
 export const environment = {
   production: true,
-  api_url: '',
+  apiUrl: '',
   mockApi: false,
   technologiesApi: `${baseApi}technologies`,
   imagesApi: `${baseApi}images`,
