85e6a696d1a50804e7ca3b1522d551f7af45f36e
Martinez Machi <fdmz@gft.com>
2019-12-10 13:29:04 +0100

    shared modules sorted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // Components
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { CreateAppModalComponent } from './components/create-app-modal/create-app-modal.component';
 import { ModalComponent } from './components/modal/modal.component';
-import { HttpClientModule } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 // Pipes
 import { SafePipe } from './pipes/safe.pipe';
+import { ReversePipe } from './pipes/reverse.pipe';
 
 import { ModalService } from './components/modal/modal.service';
-import { ReversePipe } from './pipes/reverse.pipe';
 
 const components = [AccordionComponent, CreateAppModalComponent, ModalComponent];
 
 const exports = [
   AccordionComponent,
+  AngularSvgIconModule,
   CreateAppModalComponent,
   ModalComponent,
   SafePipe,
-  AngularSvgIconModule,
 ];
 
 @NgModule({
