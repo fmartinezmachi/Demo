da6003a14bdd524f87aa6b7a1606dc5e1e274e22
Martinez Machi <fdmz@gft.com>
2019-12-13 16:18:49 +0100

    loader template and functionality

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { takeWhile } from 'rxjs/operators';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
 })
-export class LoaderComponent implements OnInit {
-  @Input() isLoading = true;
+export class LoaderComponent implements OnDestroy {
   @Input() percentage = 0;
 
-  constructor() {}
+  subscription = new Subscription();
 
-  ngOnInit() {}
+  constructor() {
+    this.runProgressBar();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  runProgressBar() {
+    this.subscription.add(
+      timer(0, 300)
+        .pipe(takeWhile(() => this.restart()))
+        .subscribe(() => {
+          this.percentage = this.percentage + 1;
+          console.log(this.percentage);
+        }),
+    );
+  }
+
+  isWidthWithinLimit() {
+    if (this.percentage === 100) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  restart = () => {
+    if (this.percentage === 100) {
+      this.percentage = 0;
+    }
+    return true;
+  };
 }
